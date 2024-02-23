ee71ffe1ecca170992b50b04a4f90a8390f523ad
zafchiel <bialakropla@proton.me>
2024-02-23 19:52:40 +0100

    dynamic font size

---
+++ src/lib/components/about.svelte
@@ -0,0 +1,12 @@
+<section>
+  <h2>About</h2>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, unde!
+    Ad illo similique perferendis atque placeat. Voluptatum fuga quam temporibus
+    consectetur neque fugit nobis dolor, dolorum quisquam sequi quos optio.
+    Harum provident velit eveniet dolor, voluptate minima aliquam cupiditate,
+    temporibus odio animi facilis similique voluptatum eius saepe nemo optio.
+    Deserunt asperiores amet, quod officia minima magni provident veritatis nisi
+    ipsam.
+  </p>
+</section>
---
+++ src/lib/components/intro.svelte
@@ -0,0 +1,7 @@
+<section>
+  <h1>
+    Zafchiel,
+    <br />
+    Fullstack Enthusiast
+  </h1>
+</section>
