4bcd1ef9ab946217dfcd5bfc4353353a3eae3f08
zafchiel <bialakropla@proton.me>
2024-02-25 20:22:59 +0100

    tech stack marquee icons

--- src/lib/components/marquee.svelte
+++ src/lib/components/marquee.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
   import avatar from "$lib/assets/avatar.png";
+  import jsIcon from "$lib/assets/tech/javascript.svg";
+  import tsIcon from "$lib/assets/tech/typescript-icon.svg";
+  import nextjsIcon from "$lib/assets/tech/nextjs.svg";
+  import nodejsIcon from "$lib/assets/tech/nodejs-icon.svg";
+  import pythonIcon from "$lib/assets/tech/python.svg";
+  import reactIcon from "$lib/assets/tech/react.svg";
+  import svelteIcon from "$lib/assets/tech/svelte-icon.svg";
+  import svelteKitIcon from "$lib/assets/tech/svelte-kit.svg";
 
   const duplicateSlide = (node: HTMLElement) => {
     const slide = node.querySelector(".slide");
@@ -13,9 +21,58 @@
 
 <div class="slider" use:duplicateSlide>
   <div class="slide">
-    <img src={avatar} alt="avatar" width="100" height="100" />
-    <img src={avatar} alt="avatar" width="100" height="100" />
-    <img src={avatar} alt="avatar" width="100" height="100" />
+    <img
+      src={jsIcon}
+      alt="avatar"
+      width="100"
+      height="100"
+      title="JavaScript"
+    />
+    <img
+      src={tsIcon}
+      alt="avatar"
+      width="100"
+      height="100"
+      title="TypeScript"
+    />
+    <img
+      src={nodejsIcon}
+      alt="avatar"
+      width="100"
+      height="100"
+      title="NodeJS"
+    />
+    <img
+      src={svelteIcon}
+      alt="avatar"
+      width="100"
+      height="100"
+      title="Svelte"
+    />
+    <img
+      src={svelteKitIcon}
+      alt="avatar"
+      width="100"
+      height="100"
+      title="Svelte Kit"
+    />
+
+    <img src={reactIcon} alt="avatar" width="100" height="100" title="React" />
+
+    <img
+      src={nextjsIcon}
+      alt="avatar"
+      width="100"
+      height="100"
+      title="NextJS"
+    />
+    <img
+      src={pythonIcon}
+      alt="avatar"
+      width="100"
+      height="100"
+      title="Python"
+    />
   </div>
 </div>
 
@@ -36,7 +93,7 @@
     width: 80px;
     top: 0;
     z-index: 3;
-    /* opacity: 0.8; */
+    pointer-events: none;
   }
 
   .slider::before {
