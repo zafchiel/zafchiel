dec9a5a36569ebb66260f1d1282256b17e1d991b
zafchiel <bialakropla@proton.me>
2024-03-01 21:20:09 +0100

    Update threshold value in intersection observer

--- src/lib/utils/intersection-observer.ts
+++ src/lib/utils/intersection-observer.ts
@@ -12,7 +12,7 @@ export const intersectionObserverAction: Action = (node) => {
             }
         }, 
         {
-            threshold: 0.1
+            threshold: 0.4
         }) 
 
         observer.observe(node);
