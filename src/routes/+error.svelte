e6f57a0f2587b2c4c4e6e2efccba37879236cfa4
zafchiel <bialakropla@proton.me>
2024-03-02 11:26:09 +0100

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<main>
+  <h1>{$page.status}: {$page.error?.message}</h1>
+  <a href="/">return to home page</a>
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    height: 100dvh;
+  }
+
+  a {
+    padding: 0.5rem 1.5rem;
+    background-color: transparent;
+    color: var(--color-primary);
+    border: 2px solid var(--color-primary);
+    border-radius: 0.5rem;
+    font-size: var(--fs-md);
+    font-weight: 600;
+    text-transform: uppercase;
+  }
+</style>
