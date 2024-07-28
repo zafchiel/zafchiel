fc4a56dc45da91ea6ec59ab74d0b2b76fffb1675
zafchiel <bialakropla@proton.me>
2024-07-28 18:54:31 +0200

    update contact section styling

--- src/lib/components/contact.svelte
+++ src/lib/components/contact.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
   import { intersectionObserverAction } from "$lib/utils/intersection-observer";
   import githubIcon from "$lib/assets/github-icon.svg";
+  import mailIcon from "$lib/assets/mail-icon.svg";
 </script>
 
 <section id="contact" use:intersectionObserverAction>
   <h2>Contact</h2>
-  <p>
-    If you have any questions, feel free to contact me at <a
-      href="mailto:j.krason@proton.me"
-      class="primary_color">j.krason@proton.me</a
-    >.
-  </p>
-  <a href="https://github.com/zafchiel" target="_blank" class="github_link">
+  <p>If you have any questions, feel free to contact me.</p>
+  <a href="mailto:j.krason@proton.me" class="social_link">
+    <img src={mailIcon} alt="mail logo" />
+    <i>j.krason@proton.me</i>
+  </a>
+  <a href="https://github.com/zafchiel" target="_blank" class="social_link">
     <img src={githubIcon} alt="github logo" />
-    <span>My github</span>
+    <i>My github</i>
   </a>
 </section>
 
@@ -27,14 +27,11 @@
     margin: 1rem 0;
   }
 
-  .primary_color {
-    color: var(--color-primary);
-  }
-
-  .github_link {
+  .social_link {
     display: flex;
     align-items: center;
     gap: 0.5rem;
     color: var(--color-primary);
+    margin: 1rem 0.5rem;
   }
 </style>
